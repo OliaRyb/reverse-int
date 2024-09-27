cd58aa3c912a6dc991de757f5cf003a573bfebce
OliaRyb <olya.rybakova.3004@gmail.com>
2024-09-27 16:41:22 +0400

    feat: add reverse feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let revArray = [];
+  let i = Math.abs(n);
+  while (i > 0) {
+    let k = i % 10;
+    revArray.push(k);
+    i = Math.trunc(i / 10);
+  }
+  let newNum = parseInt(revArray.join(''), 10);
+  console.log(newNum);
+  return (newNum);
 }
